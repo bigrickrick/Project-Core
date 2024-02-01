fb589c8376184299144c262d215125671512dafe
bigrickrick <122563862+bigrickrick@users.noreply.github.com>
2024-02-01 10:07:14 -0500

    made so that you could look up and down

--- Project Core/Assets/Scripts/PlayerCam.cs
+++ Project Core/Assets/Scripts/PlayerCam.cs
@@ -30,7 +30,7 @@ public class PlayerCam : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         
-        camHolder.rotation = Quaternion.Euler(0, yRotation, 0);
+        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
 
     }
